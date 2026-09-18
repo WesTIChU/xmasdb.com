ebfb6eb1f600903026d2b979f85678d022f631c1
WesTIChU <webmaster@neellik.com>
2026-09-18 16:30:26 +0100

    Remove obsolete scripts and backup files

--- scripts/debug-pipeline.js
+++
@@ -1,7 +0,0 @@
-import { inspectMovieCast, rebuildAllCastFromTmdb } from './enrich-cast.js';
-
-const run = process.argv.includes('--rebuild') ? rebuildAllCastFromTmdb : inspectMovieCast;
-run({}).catch(err => {
-  console.error(err.message);
-  process.exitCode = 1;
-});
